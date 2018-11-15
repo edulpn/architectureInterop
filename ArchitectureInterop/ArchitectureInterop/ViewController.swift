e6880f9cd0174363d3d86480297abd15f07a4625
Eduardo Pinto <eduardoneto@tokenlab.com.br>
2018-11-15 15:27:41 -0200

    Initial Commit

---
+++ ArchitectureInterop/ArchitectureInterop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureInterop
+//
+//  Created by Eduardo Pinto on 11/15/18.
+//  Copyright © 2018 Eduardo Pinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
